9e07a221ad666fbbc764001ee1aaa5a953dc5449
Pierre Ardoin <32256817+mapiolca@users.noreply.github.com>
2026-02-24 23:03:41 +0100

    Affiche Retour au brouillon aussi pour le statut Envoyé

--- diffusion_card.php
+++ diffusion_card.php
@@ -699,8 +699,11 @@ if ($object->id > 0 && (empty($action) || ($action != 'edit' && $action != 'crea
 			}
 
 			// Back to draft
-			if ($object->status == $object::STATUS_VALIDATED) {
+			if ($object->status == $object::STATUS_VALIDATED || $object->status == $object::STATUS_SENT) {
 				print dolGetButtonAction('', $langs->trans('SetToDraft'), 'default', $_SERVER["PHP_SELF"].'?id='.$object->id.'&action=confirm_setdraft&confirm=yes&token='.newToken(), '', $permissiontoadd);
+			}
+
+			if ($object->status == $object::STATUS_VALIDATED) {
 				print dolGetButtonAction('', $langs->trans('MarkAsSent'), 'default', $_SERVER['PHP_SELF'].'?id='.$object->id.'&action=set_sent&token='.newToken(), '', $permissiontoadd);
 			}
 
